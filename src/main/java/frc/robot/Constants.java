90bc17d7deb1aba2a441a8eb51126a5c787cb190
adarshpay <adarsh.payyakkil@gmail.com>
2023-04-07 15:03:27 -0600

    Changed shelf preset

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -100,7 +100,7 @@ public final class Constants {
     // Shelf intaking
     public static final double SHELF_FRONTSIDE_ARM_ROTATION = 161;
     public static final double SHELF_FRONTSIDE_WRIST_ROTATION = 68 + wristOffsetDiffArm;
-    public static final double SHELF_ARM_EXTENSION = 18.2;
+    public static final double SHELF_ARM_EXTENSION = 19;
 
     // BACK SIDE (worm gear)
     // High row placement
